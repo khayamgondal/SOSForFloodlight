50c525d90884fcc49f3a0ec7c0548d925386e031
Khayam Gondal <khayam.gondal@gmail.com>
2017-09-26 21:55:20 -0400

    controller will be started in screen

---
+++ start_controller.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#author Khayam Anjam kanjam@g.clemson.edu
+#Easy script to start controller. Run it on your controller node (Vagrant VM)
+#Note! it will start the controller inside the screen
+
+cd  /home/ubuntu/sos-for-floodlight
+screen -S floodlight java -jar target/floodlight.jar
